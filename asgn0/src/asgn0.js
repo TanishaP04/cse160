3dd853f31cc06fffe4cb3567c2d9e8bb23d65a11
TanishaP04 <taprabhu@ucsc.edu>
2026-01-08 03:17:38 +0000

    .

--- asgn0/src/asgn0.js
+++ asgn0/src/asgn0.js
@@ -14,8 +14,8 @@ function main(){
     //ctx.fillRect(120,10,150,150); //fill a rectangle with the color
 
     //make background black
-    //ctx.fillStyle = 'black';
-    //ctx.fillRect(0,0,400,400);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,400,400);
 
     // red line
     let v1 = new Vector3([2.25, 2.25, 0]);
@@ -27,9 +27,6 @@ function drawVector(v,color){
     ctx.lineWidth = 2;
     ctx.beginPath();
     ctx.moveTo(200,200);
-    ctx.lineTo(
-        200 + v.elements[0] * 20,
-        200 - v.elements[1] * 20
-    );
+    ctx.lineTo(200 + v.elements[0] * 20, 200 - v.elements[1] * 20);
     ctx.stroke();
 }
