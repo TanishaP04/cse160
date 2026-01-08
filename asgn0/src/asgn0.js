bdb00cc5518f549bfefda63558b2e7408697a737
TanishaP04 <taprabhu@ucsc.edu>
2026-01-08 03:14:55 +0000

    .

--- asgn0/src/asgn0.js
+++ asgn0/src/asgn0.js
@@ -1,40 +1,35 @@
-let ctx; // make ctx global
-
-function main() {
-    // Retrieve <canvas> element
+let ctx; //make ctx global
+function main(){
+    //Retrieve <canvas> element 
     var canvas = document.getElementById('example');
-    if (!canvas) {
+    if(!canvas){
         console.log('Failed to retrieve the <canvas> element');
         return;
     }
-
-    // Get 2D rendering context
+    //Get the rendering context for 2DCG (assign to global `ctx`)
     ctx = canvas.getContext("2d");
 
-    // Make background black
+    //Draw a blue rectangle
+    //ctx.fillStyle = 'rgba(0,0,255,1.0)'; //set blue color
+    //ctx.fillRect(120,10,150,150); //fill a rectangle with the color
+
+    //make background black
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, 400, 400);
+    ctx.fillRect(0,0,400,400);
 
-    // Instantiate vector v1 = (2.25, 2.25, 0)
+    // red line
     let v1 = new Vector3([2.25, 2.25, 0]);
-
-    // Draw vector in red
     drawVector(v1, "red");
 }
 
-function drawVector(v, color) {
+function drawVector(v,color){
     ctx.strokeStyle = color;
     ctx.lineWidth = 2;
-
     ctx.beginPath();
-    // Origin at center of canvas
-    ctx.moveTo(200, 200);
-
-    // Scale vector by 20 and invert y-axis
+    ctx.moveTo(200,200);
     ctx.lineTo(
         200 + v.elements[0] * 20,
         200 - v.elements[1] * 20
     );
-
     ctx.stroke();
 }
