8061d91c87f33aa0162ee1d7af0770230258a59a
TanishaP04 <taprabhu@ucsc.edu>
2026-01-08 01:43:02 +0000

    .

--- asgn0/src/asgn0.js
+++ asgn0/src/asgn0.js
@@ -10,9 +10,13 @@ function main(){
     ctx = canvas.getContext("2d");
 
     //Draw a blue rectangle
-    ctx.fillStyle = 'rgba(0,0,255,1.0)'; //set blue color
-    ctx.fillRect(120,10,150,150); //fill a rectangle with the color
+    //ctx.fillStyle = 'rgba(0,0,255,1.0)'; //set blue color
+    //ctx.fillRect(120,10,150,150); //fill a rectangle with the color
 
+    //make background black
+    ctx.fillStyle = 'rgba(0,0,0,1.0)';
+    ctx.fillRect(0,0,400,400);
+    
     // red line
     let v1 = new Vector3([2.25, 2.25, 0]);
     drawVector(v1, "red");
